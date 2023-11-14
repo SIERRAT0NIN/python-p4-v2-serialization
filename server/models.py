557566a1f0b6cc068f71968275219c4a6534b1b8
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-11-14 15:06:44 -0800

    done

--- server/models.py
+++ server/models.py
@@ -7,12 +7,22 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 
-class Pet(db.Model):
+class Pet(db.Model, SerializerMixin):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+    body = {'count': len(pets),
+            'pets': pets
+            }
+    return make_response(body, 200)
 
     def __repr__(self):
         return f'<Pet {self.id}, {self.name}, {self.species}>'
